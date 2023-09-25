7768c69c323acb68fcc5ba26bb33389ac2f65826
Eduarda Medeiros <eduarda_medeiros_silva@hotmail.com>
2023-09-25 13:23:43 -0300

    novas atualizacoes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 function atualizarpag(pagina) {
     window.location = pagina + ".html";
 }
+
+function show_question (n) {
+    if (document.getElementById(n).style.display == 'none'){
+        document.getElementById(n).style.display = 'block'; 
+        document.getElementById('q'+ n).style.backgroundImage = 'url(../img/acima.png)';
+    } else {
+        document.getElementById(n).style.display = 'none'; 
+        document.getElementById('q'+ n).style.backgroundImage = 'url(../img/abaixo.png)';
+    }
+}
+
+function show_questionIndex (n) {
+    if (document.getElementById(n).style.display == 'none'){
+        document.getElementById(n).style.display = 'block'; 
+        document.getElementById('q'+ n).style.backgroundImage = 'url(img/acima.png)';
+    } else {
+        document.getElementById(n).style.display = 'none'; 
+        document.getElementById('q'+ n).style.backgroundImage = 'url(img/abaixo.png)';
+    }
+}
