c76c504962b74cfe954b7d31dc3caf200d943a97
Eduarda Medeiros <eduarda_medeiros_silva@hotmail.com>
2023-11-02 04:18:05 -0300

    FAQ 2.0

--- js/script.js
+++ js/script.js
@@ -2,22 +2,46 @@ function atualizarpag(pagina) {
     window.location = pagina + ".html";
 }
 
-function show_question (n) {
-    if (document.getElementById(n).style.display == 'none'){
-        document.getElementById(n).style.display = 'block'; 
-        document.getElementById('q'+ n).style.backgroundImage = 'url(../img/acima.png)';
-    } else {
-        document.getElementById(n).style.display = 'none'; 
-        document.getElementById('q'+ n).style.backgroundImage = 'url(../img/abaixo.png)';
-    }
+let navbar = document.querySelector(".navbar");
+
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function () {
+    navLinks.style.right = "0";
+}
+menuCloseBtn.onclick = function () {
+    navLinks.style.right = "-100%";
 }
 
-function show_questionIndex (n) {
-    if (document.getElementById(n).style.display == 'none'){
-        document.getElementById(n).style.display = 'block'; 
-        document.getElementById('q'+ n).style.backgroundImage = 'url(img/acima.png)';
-    } else {
-        document.getElementById(n).style.display = 'none'; 
-        document.getElementById('q'+ n).style.backgroundImage = 'url(img/abaixo.png)';
-    }
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function () {
+    navLinks.classList.toggle("show1");
 }
+
+const accordionItemHeaders = document.querySelectorAll(
+    ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+    accordionItemHeader.addEventListener("click", (event) => {
+        const currentlyActiveAccordionItemHeader = document.querySelector(
+            ".accordion-item-header.active"
+        );
+        if (
+            currentlyActiveAccordionItemHeader &&
+            currentlyActiveAccordionItemHeader !== accordionItemHeader
+        ) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    });
+});
+
