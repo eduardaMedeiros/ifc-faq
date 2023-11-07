eaaee8145c42bd0e77de428b7a5f2e3c437d4142
Eduarda Medeiros <eduarda_medeiros_silva@hotmail.com>
2023-11-07 16:40:54 -0300

    formulario

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
+//funcao atualizar pagina
 function atualizarpag(pagina) {
     window.location = pagina + ".html";
 }
 
+//funcoes do header
 let navbar = document.querySelector(".navbar");
-
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+
 menuOpenBtn.onclick = function () {
     navLinks.style.right = "0";
 }
@@ -19,6 +21,7 @@ htmlcssArrow.onclick = function () {
     navLinks.classList.toggle("show1");
 }
 
+//funcoes dos itens de duvidas
 const accordionItemHeaders = document.querySelectorAll(
     ".accordion-item-header"
 );
@@ -45,3 +48,46 @@ accordionItemHeaders.forEach((accordionItemHeader) => {
     });
 });
 
+const button = document.querySelector('#btn-enviar');
+
+const addLoading = () =>{
+    button.innerHTML = '<div class="loading"></div>';
+}
+
+const removeLoading = () =>{
+    button.innerHTML = 'Dados enviados!';
+
+    document.querySelector('input[name=Nome]').value = '';
+    document.querySelector('input[name=Email]').value = '';
+    document.querySelector('input[name=Distribuidor]').value = '';
+    document.querySelector('input[name=Loja]').value = '';
+    document.querySelector('textarea[name=Observacao]').value = '';
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    addLoading();
+
+    const nome = document.querySelector('input[name=Nome]').value;
+    const email = document.querySelector('input[name=Email]').value;
+    const dist = document.querySelector('input[name=Distribuidor]').value;
+    const loja = document.querySelector('input[name=Loja]').value;
+    const obs = document.querySelector('textarea[name=Observacao]').value;
+
+    fetch('https://api.sheetmonkey.io/form/928Za6QB7wbk1jFUZRZeQv',{
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            Nome: nome,
+            Email: email,
+            Distribuidor: dist,
+            Loja: loja,
+            Observacao: obs,
+        })
+    }).then(() => removeLoading());
+}
+
+document.querySelector('#form-contato').addEventListener('submit', handleSubmit);
